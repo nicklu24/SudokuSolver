78b0f52fdb189fe62c51d4a87ee3dbdc2e09a119
nicklu24 <nlucido@okstate.edu>
2015-04-07 18:55:19 -0500

    added 2nd initialize function

---
+++ initializeBoard2.m
@@ -0,0 +1,61 @@
+function [sudoku] = initializeBoard2(inSudoku)
+% Initialize
+sudoku = ones(9,9,11);
+sudoku(:,:,10:11)=0;
+iRange=[0 0];
+jRange=[0 0];
+ 
+% We know everything 9th entry is a new row, so floor(entry#/9)=row.  Also
+% the column position is roughly entry# mod 9 (the column case is special
+% case)
+for m  = 1:81
+    i = ceil(m/9);
+    j = mod(m,9);
+    if (j==0) 
+        j=9;
+    end % end if        
+    sudoku(i,j,11)=inSudoku(m);
+end % end m loop
+    
+
+
+for i=1:9
+    for j= 1:9
+        if (sudoku(i,j,11))
+            % Determine iRange for the 3x3 box
+            switch i
+                case {1,2,3}
+                    iRange = [1 3];
+                case {4,5,6}
+                    iRange = [4 6];
+                case {7,8,9}
+                    iRange = [7 9];
+            end
+            % Determine jRange for the 3x3 box
+            switch j
+                case {1,2,3}
+                    jRange = [1 3];
+                case {4,5,6}
+                    jRange = [4 6];
+                case {7,8,9}
+                    jRange = [7 9];
+            end % End j loop
+            sudoku(i,j,(1:9)) = 0;                        % zero out the z column for this spot
+            sudoku(i,:,(sudoku(i,j,11))) = 0;            % zero out the j column for bitboard z
+            sudoku(:,j,(sudoku(i,j,11))) = 0;            % zero out the i row for bitboard z
+            sudoku(iRange(1) : iRange(2), jRange(1) : jRange(2), sudoku(i,j,11)) = 0;        % zero out the box
+            sudoku(i,j,(sudoku(i,j,11))) = 1;            % Put the the 1 where it belongs
+            
+        end % End if
+    end % End j loop
+end % End i loop
+
+% Sums the z columns 
+for i=1:9 
+    for j=1:9;
+        sudoku(i,j,10) = sum(sudoku(i,j,1:9));
+    end % End j loop
+end % End i loop
+
+disp(sudoku)
+end
