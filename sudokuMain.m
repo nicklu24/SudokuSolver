f61b92fcd4bdd2720509f9a2f99b0e2f0d3f2de7
grantmwilliams <grant.williams@okstate.edu>
2015-04-06 22:05:06 -0500

    basic outline finished

--- sudokuMain.m
+++ sudokuMain.m
@@ -2,6 +2,25 @@
 clear;close all;clc;
 tic
 
+solved = false;
+iteration = 0;
+% Initialize Board
 initializeBoard();
+
+while (solved == false)
+    % Search
+    
+    % Backtrack
+    
+    % Check Solved
+    
+    % Increment
+    iteration = iteration + 1;
+    if (iteration == 1e6)
+        break
+    end
+end
+
+% Print Board
 
 toc
