69c3618ea244885c60828a23e404cf799e365212
nicklu24 <nlucido@okstate.edu>
2015-04-07 01:03:43 -0500

    Added input section of code
    also included disp(sudoku) for easy error checking

--- initializeBoard.m
+++ initializeBoard.m
@@ -6,8 +6,26 @@ possum = zeros(9,9);
 iRange=[0 0];
 jRange=[0 0];
 
+
 % Enter the known elements of the sudoku array
-sudoku(4,3)=9;
+% sudoku used available at this link (http://www.websudoku.com/?level=2&set_id=5989254854)
+inSudoku = [4 0 0 0 0 0 7 0 9 0 0 9 0 0 5 0 0 3 0 8 0 1 9 7 0 0 0 0 0 0 0 3 0 0 0 6 1 0 0 4 0 9 0 0 2 5 0 0 0 6 0 0 0 0 0 0 0 2 7 3 0 6 0 9 0 0 6 0 0 8 0 0 2 0 7 0 0 0 0 0 4];
+
+% We know everything 9th entry is a new row, so floor(entry#/9)=row.  Also
+% the column position is roughly entry# mod 9 (the column case is special
+% case)
+for m  = 1:81
+    i = ceil(m/9);
+    j = mod(m,9);
+    if (j==0) 
+        j=9;
+    end % end if        
+    sudoku(i,j)=inSudoku(m);
+end % end m loop
+
+
+disp(sudoku)    
+
 
 for i=1:9
     for j= 1:9
@@ -33,7 +51,7 @@ for i=1:9
             possible(i,j,:) = 0;                        % zero out the z column for this spot
             possible(i,:,(sudoku(i,j))) = 0;            % zero out the j column for bitboard z
             possible(:,j,(sudoku(i,j))) = 0;            % zero out the i row for bitboard z
-            possible(iRange(1) : iRange(2), jRange(1) : jRange(2), sudoku(i,j)) = 0;        %zero out the box
+            possible(iRange(1) : iRange(2), jRange(1) : jRange(2), sudoku(i,j)) = 0;        % zero out the box
             possible(i,j,(sudoku(i,j))) = 1;            % Put the the 1 where it belongs
             
         end % End if
@@ -45,6 +63,7 @@ for i=1:9
         possum(i,j) = sum(possible(i,j,:));
     end % End j loop
 end % End i loop
+
 
 end
 
